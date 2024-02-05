61e884a46ac03e9c0075c1aef6d95cb83b923093
rafacoper <rafaelbarbosa.tecnologia@outlook.com>
2024-02-05 17:23:30 -0300

    Acelerando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,26 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { BlogsComponent } from './blogs/blogs.component';
 import { HeaderComponent } from './header/header.component';
+import { TecnologyComponent } from './tecnology/tecnology.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, BlogsComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    BlogsComponent,
+    FooterComponent,
+    TecnologyComponent,
+    ContactComponent,
+    NotFoundComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Rafe Steps';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { BlogsComponent } from './blogs/blogs.component';
+import { TecnologyComponent } from './tecnology/tecnology.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'blog', component: BlogsComponent },
+  { path: 'technology', component: TecnologyComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '', redirectTo: '/blog', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
+];
